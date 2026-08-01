ca05fa22de080cf9f1f821f461179950c86a45fd
nikhil6393 <nikhil900285@gmail.com>
2026-08-01 22:14:31 +0530

    Time: 2 ms (81.64%) | Memory: 46.6 MB (65.35%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,84 @@
+class Solution {
+
+    List<List<String>> ans = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) {
+
+        char[][] chessBoard = new char[n][n];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                chessBoard[i][j] = '.';
+
+        nQueens(0, chessBoard);
+
+        return ans;
+    }
+
+    private void nQueens(int row, char[][] chessBoard) {
+
+        int n = chessBoard.length;
+
+        if (row == n) {
+
+            List<String> temp = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                temp.add(new String(chessBoard[i]));
+            }
+
+            ans.add(temp);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+
+            if (canQueenBePlaced(row, col, chessBoard)) {
+
+                chessBoard[row][col] = 'Q';   // mark
+
+                nQueens(row + 1, chessBoard);
+
+                chessBoard[row][col] = '.';   // unmark
+            }
+        }
+    }
+
+    private boolean canQueenBePlaced(int row, int col, char[][] chessBoard) {
+
+        int n = chessBoard.length;
+
+        // check column
+        int i = row - 1;
+
+        while (i >= 0) {
+            if (chessBoard[i][col] == 'Q')
+                return false;
+            i--;
+        }
+
+        // check left diagonal
+        i = row - 1;
+        int j = col - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (chessBoard[i][j] == 'Q')
+                return false;
+            i--;
+            j--;
+        }
+
+        // check right diagonal
+        i = row - 1;
+        j = col + 1;
+
+        while (i >= 0 && j < n) {
+            if (chessBoard[i][j] == 'Q')
+                return false;
+            i--;
+            j++;
+        }
+
+        return true;
+    }
+}
